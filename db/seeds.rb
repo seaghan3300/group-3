9281c64ac247c0c3e83b1a69d289c4e8f54fd443
Seaghan3300 <sennis@uccs.edu>
2020-09-24 21:48:31 +0000

    sprint 0.3.2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+some_properties = [{:address => '1324 Place', :price => '$12312', :condition => 'Medium', :footage => '1500', :bedrooms => '3', :bathrooms=> '2', :rating => '7'}]
+
+some_properties.each do |properties|
+  Property.create!(properties)
+end
